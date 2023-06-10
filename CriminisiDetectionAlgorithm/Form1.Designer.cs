b21ba5e58a56f6036eb27ff99da557b242817ef3
iuli18icl <iuliana.clotan@yahoo.com>
2023-06-10 22:11:51 +0300

    M1

--- CriminisiDetectionAlgorithm/Form1.Designer.cs
+++ CriminisiDetectionAlgorithm/Form1.Designer.cs
@@ -43,12 +43,17 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(1, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(874, 646);
             this.pictureBox1.TabIndex = 0;
@@ -57,7 +62,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 720);
+            this.button1.Location = new System.Drawing.Point(28, 769);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(184, 128);
             this.button1.TabIndex = 1;
@@ -67,7 +72,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1199, 76);
+            this.textBox1.Location = new System.Drawing.Point(772, 769);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(130, 31);
             this.textBox1.TabIndex = 2;
@@ -76,7 +81,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1216, 48);
+            this.label1.Location = new System.Drawing.Point(788, 741);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 25);
             this.label1.TabIndex = 4;
@@ -94,7 +99,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1050, 184);
+            this.label3.Location = new System.Drawing.Point(1066, 741);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 25);
             this.label3.TabIndex = 6;
@@ -104,7 +109,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1050, 357);
+            this.label4.Location = new System.Drawing.Point(1065, 907);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 25);
             this.label4.TabIndex = 7;
@@ -113,7 +118,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1398, 357);
+            this.label5.Location = new System.Drawing.Point(1342, 907);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(74, 25);
             this.label5.TabIndex = 8;
@@ -122,7 +127,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(1398, 184);
+            this.label6.Location = new System.Drawing.Point(1342, 741);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(67, 25);
             this.label6.TabIndex = 9;
@@ -130,7 +135,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(1023, 212);
+            this.textBox2.Location = new System.Drawing.Point(1040, 769);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(130, 31);
             this.textBox2.TabIndex = 10;
@@ -138,7 +143,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(1023, 385);
+            this.textBox3.Location = new System.Drawing.Point(1040, 935);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(130, 31);
             this.textBox3.TabIndex = 11;
@@ -146,7 +151,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(1369, 212);
+            this.textBox4.Location = new System.Drawing.Point(1308, 769);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(130, 31);
             this.textBox4.TabIndex = 12;
@@ -154,7 +159,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(1369, 385);
+            this.textBox5.Location = new System.Drawing.Point(1308, 935);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(130, 31);
             this.textBox5.TabIndex = 13;
@@ -163,7 +168,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(1206, 526);
+            this.label7.Location = new System.Drawing.Point(788, 907);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(108, 25);
             this.label7.TabIndex = 14;
@@ -171,16 +176,57 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(1199, 554);
+            this.textBox6.Location = new System.Drawing.Point(772, 935);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(130, 31);
             this.textBox6.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1998, 769);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 128);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Load Mask";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1012, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 128);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Compute";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Compute_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1308, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(874, 646);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1040, 477);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 33);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1992, 1274);
+            this.ClientSize = new System.Drawing.Size(2212, 1274);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox5);
@@ -200,6 +246,7 @@
             this.Text = "Criminisi Detection";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -222,6 +269,10 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- CriminisiDetectionAlgorithm/FuzyListCompute .cs
+++ CriminisiDetectionAlgorithm/FuzyListCompute .cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CriminisiDetectionAlgorithm
-{
-    public static FuzzyList ComputeFuzyList()
-    {
-        FuzzyList fuzzyList = new FuzzyList()
-        {
-            CreateFuzzyMember = new List<double>()
-        };
+//namespace CriminisiDetectionAlgorithm
+//{
+//    public static FuzzyList ComputeFuzyList()
+//    {
+//        FuzzyList fuzzyList = new FuzzyList()
+//        {
+//            CreateFuzzyMember = new List<double>()
+//        };
 
-        double endi = Math.Pow(fuzzyList.patchsize, 2);
+//        double endi = Math.Pow(fuzzyList.patchsize, 2);
 
-        for(int i = 0; i<= endi; i++)
-        {
-            if (i <= fuzzyList.a)
-            {
-                fuzzyList.CreateFuzzyMember.Add(0);
-            } 
+//        for(int i = 0; i<= endi; i++)
+//        {
+//            if (i <= fuzzyList.a)
+//            {
+//                fuzzyList.CreateFuzzyMember.Add(0);
+//            } 
             
-            else if (i > fuzzyList.b)
-            {
-                fuzzyList.CreateFuzzyMember.Add(1);
-            } 
+//            else if (i > fuzzyList.b)
+//            {
+//                fuzzyList.CreateFuzzyMember.Add(1);
+//            } 
             
-            else
-            {
-                double member = (i - fuzzyList.a) * 1.0 / (fuzzyList.b - fuzzyList.a);
-                fuzzyList.CreateFuzzyMember.Add(member);
-            }
+//            else
+//            {
+//                double member = (i - fuzzyList.a) * 1.0 / (fuzzyList.b - fuzzyList.a);
+//                fuzzyList.CreateFuzzyMember.Add(member);
+//            }
 
-        }
-        return fuzzyList;
+//        }
+//        return fuzzyList;
 
-    }
-}
+//    }
+//}
